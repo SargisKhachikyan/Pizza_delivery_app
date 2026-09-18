b0a2e5e24d20ff8543f2c8acc4b8da7db693e3ec
saqo <saqo@saqos-MacBook-Air.local>
2026-09-19 02:01:16 +0400

    added firebase login with email/password,create the login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:decision_jar_project/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'data/pizza_data.dart';
 import 'presentation/state/pizza_bloc.dart';
 import 'presentation/home_screen/home_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
